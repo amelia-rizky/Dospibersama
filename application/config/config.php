f4c75879e8a8cfd0e7b97b8bb000117eeb9f338a
amelia-rizky <62246858+amelia-rizky@users.noreply.github.com>
2020-04-07 15:04:52 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ date_default_timezone_set("Asia/Jakarta");
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/Dospi/';
+$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/Dospibersama/';
 /*
 |--------------------------------------------------------------------------
 | Index File
