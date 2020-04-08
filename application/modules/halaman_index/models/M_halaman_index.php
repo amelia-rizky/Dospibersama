54ef21bd59008cd9969997a865b9383ebcd8ad05
amelia-rizky <62246858+amelia-rizky@users.noreply.github.com>
2020-04-08 19:34:38 +0700

    syarat ketentuan

--- application/modules/halaman_index/models/M_halaman_index.php
+++ application/modules/halaman_index/models/M_halaman_index.php
@@ -5,7 +5,15 @@ class M_halaman_index extends CI_Model {
 
 	function tampil()
 	{
-		return $this->db->get('data_hewan')->result();
+		return $this->db->get('tb_index')->result();
+			// join
+			$this->db->select('*')
+				->from('tb_index')
+				->join('tb_tentang', 'tb_index.id_tentang = tb_tentang.id_tentang')
+				->join('tb_layanan', 'tb_index.id_layanan = tb_layanan.id_layanan')
+				->where('tb_index.id_index',$id_index);
+			$query = $this->db->get();
+			return $query;
     }
     
 
